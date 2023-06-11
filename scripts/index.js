bb1a238c91fb5913f49531920aacda75e6cd64c0
edowon0623@gmail.com <Dwlee$09>
2023-06-11 21:08:11 +0900

    updated the newest files

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hi, there!');
+    res.send('Hlleo, World!');
 });
 
 app.listen(8080, () => {
